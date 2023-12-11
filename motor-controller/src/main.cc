781a84f8b5b687b8b21bbe18390e3c20a5ce9191
Jasper Miller-Waugh <contact@jmw.nz>
2023-12-11 20:12:03 +1300

    Removed reverse support to streamline code

--- motor-controller/src/main.cc
+++ motor-controller/src/main.cc
@@ -6,14 +6,22 @@ constexpr int RELAY_PIN = 13;
 
 /** The midpoint of the input ADC, repersenting the stationary
  * position of the controller */
-constexpr int HALF_ADC = 0b0111111111;
+constexpr int FULL_ADC = 0b1111111111;
+// constexpr int ADC_MIDPOINT = FULL_ADC * 0.44;
+constexpr int ADC_MIDPOINT = FULL_ADC * 0.49;
 /** The threshold either side of input ADC to be used as deadzone */
-constexpr int ADC_DEADZONE = 50;
+constexpr int ADC_DEADZONE = FULL_ADC * 0.1;
+
+constexpr int ADC_MIN = FULL_ADC * 0.2;
+constexpr int ADC_MAX = FULL_ADC * 1;
 
 /** The minimum output of the PWM motor */
-constexpr int MOTOR_OUT_MIN = 50;
+constexpr int MOTOR_OUT_MIN = 114;
+// constexpr int MOTOR_OUT_MIN = 103;
+
 /** The maximim output of the motor */
-constexpr int MOTOR_OUT_MAX = 160;
+// constexpr int MOTOR_OUT_MAX = 140;
+constexpr int MOTOR_OUT_MAX = 155;
 
 /** Mode for relay to drive forwards */
 constexpr int FORWARDS_STATE = HIGH;
@@ -23,7 +31,6 @@ constexpr int BACKWARDS_STATE = LOW;
 enum MotorState { STATIONARY, FORWARDS, BACKWARDS };
 
 MotorState previous_direction = STATIONARY;
-unsigned long last_time;
 bool is_waiting = false;
 
 /**
@@ -31,16 +38,20 @@ bool is_waiting = false;
  * @param value The ADC reading, and where the PWM value will stored
  */
 inline MotorState direction_from_value(int &value) {
-  constexpr int back_det = HALF_ADC - ADC_DEADZONE;
-  constexpr int forward_det = HALF_ADC + ADC_DEADZONE;
-  constexpr float range = HALF_ADC - ADC_DEADZONE;
+  constexpr int back_det = ADC_MIDPOINT - ADC_DEADZONE;
+  constexpr int forward_det = ADC_MIDPOINT + ADC_DEADZONE;
   constexpr int multiplier = MOTOR_OUT_MAX - MOTOR_OUT_MIN;
 
   if (value < back_det) {
+    const int range = back_det - ADC_MIN;
     value = ((float)(back_det - value) / range) * multiplier + MOTOR_OUT_MIN;
+    Serial.println(value);
     return BACKWARDS;
   } else if (forward_det < value) {
+    const int range = ADC_MAX - forward_det;
     value = ((float)(value - forward_det) / range) * multiplier + MOTOR_OUT_MIN;
+    Serial.println(value);
+
     return FORWARDS;
   }
 
@@ -64,17 +75,6 @@ inline void set_direction(MotorState direction) {
   }
 }
 
-void setup_wait() {
-  if (!is_waiting) {
-    is_waiting = true;
-    last_time = millis();
-    return;
-  }
-
-  if (last_time > 1000) {
-    is_waiting = false;
-  }
-}
 
 void setup() {
   pinMode(IN_PIN, INPUT);
@@ -83,27 +83,16 @@ void setup() {
 
   Serial.begin(9600);
 
-  last_time = millis();
 }
 
 void loop() {
   int value = analogRead(IN_PIN);
+  Serial.print("Raw Value: ");
+  Serial.print((float)value / FULL_ADC);
+  Serial.print(" | ");
   MotorState current_direction = direction_from_value(value);
-
-  if (current_direction == STATIONARY) {
-    previous_direction = STATIONARY;
-    setup_wait();
-    analogWrite(OUT_PIN, 0);
-  }
-
-  if (current_direction != previous_direction) {
-    setup_wait();
-    if (is_waiting) {
-      analogWrite(OUT_PIN, 0);
-      return;
-    }
-    set_direction(current_direction);
-  }
-
+  Serial.print(value);
+  Serial.print(" | ");
+  Serial.println(current_direction);
   analogWrite(OUT_PIN, value);
 }
